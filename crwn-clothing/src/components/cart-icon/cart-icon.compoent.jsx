8b32b6921c6c18ae7621348360999e0b07d1e2af
smishra0722 <s.mishra0722@gmail.com>
2021-01-28 11:00:22 +0530

    added functionality fo displaying cart items and total items is displayed on cart icon

--- crwn-clothing/src/components/cart-icon/cart-icon.compoent.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.compoent.jsx
@@ -4,15 +4,18 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({toggleCartHidden, cartItems}) => {
     return (
         <div className='cart-icon' onClick={toggleCartHidden}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'> 0 </span>
+            <span className='item-count'> {cartItems.length} </span>
         </div>
     );
 }
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+});
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
